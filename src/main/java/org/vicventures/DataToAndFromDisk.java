1481dd524222c9c3ccec43e61cfd3832a8976a9e
Victor Harbo Johnston <vholesen@hotmail.com>
2023-03-21 14:51:41 +0100

    add new files

---
+++ src/main/java/org/vicventures/DataToAndFromDisk.java
@@ -0,0 +1,37 @@
+package org.vicventures;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataToAndFromDisk {
+
+    public static void mapToDisk(Map<String, Map<String, Integer>> output){
+        try {
+            FileOutputStream fileOut =
+                    new FileOutputStream("src/main/resources/data/mapOfOdddernettetData");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(output);
+            out.close();
+            fileOut.close();
+            System.out.printf("src/main/resources/data/mapOfOdddernettetData");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+
+    public static Map<String, Map<String, Integer>> mapFromDisk(){
+        Map<String, Map<String, Integer>> loadedMap = null;
+        try {
+            FileInputStream fileIn = new FileInputStream("src/main/resources/data/mapOfOdddernettetData");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            loadedMap = (Map<String, Map<String, Integer>>) in.readObject();
+            in.close();
+            fileIn.close();
+            return loadedMap;
+        } catch (IOException | ClassNotFoundException i) {
+            i.printStackTrace();
+        }
+        return loadedMap;
+    }
+}
